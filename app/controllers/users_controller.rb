3cb410b65eb0cae74244b18f4aa8854dd6c37f7e
Aperryhu <aperryhu@chicagobooth.edu>
2022-09-19 20:21:00 +0000

    signin, signup, signout finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,41 @@
 class UsersController < ApplicationController
+
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    #look up the record from the db matching username
+    user = User.where({:username => un}).at(0)
+
+    #if there's no record, redirect to sign in form
+
+    if user == nil
+    redirect_to("/user_sign_in", {:alert => "No one named that round these parts. Sign Up fool!"})
+
+    # if there is a record, check to see that pw matches
+     else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+      redirect_to("/", {:notice => "Welcome back " +user.username})
+      else
+        redirect_to("/user_sign_in", {:alert => "Nah Bruh, Try again"})
+      end
+    end
+  end
  
   def new_registration_form
     render({ :template => "users/signup_form.html.erb" })
   end
  
- 
+  def new_session_form
+    render({ :template => "users/signin_form.html.erb" })
+  end
+
+
+ def toast_cookies
+  reset_session
+  redirect_to("/", :notice => "See ya later")
+ end
  
   def index
     @users = User.all.order({ :username => :asc })
@@ -29,6 +60,8 @@ class UsersController < ApplicationController
     save_status = user.save
 
     if save_status == true
+    session.store(:user_id, user.id)
+
     redirect_to("/users/#{user.username}", {:notice => "Welcome, " + user.username + "!"})
     else
     redirect_to("/user_sign_up", {:alert => user.errors.full_messages.to_sentence})
